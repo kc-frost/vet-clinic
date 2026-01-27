f65efad0fdd64328ba190b465f7430f1ebe0b8ce
k128c <aeth.kr@gmail.com>
2026-01-26 23:04:00 -0600

    Add password hashing and connection to a db

--- registration.py
+++ registration.py
@@ -1,34 +1,94 @@
 # basic setup
 # temporarily assumes that a new email is to be typed, rather than retrieved from a http post request
 
-# built-in regex package (no need for import)
+# database variables
+USER = "root"
+PASSWORD = ""
+HOST = "localhost"
+DATABASE = ""
+
+# built-in (python 3.0<=)
 import re
+import hashlib
 
-# email validation
-# the below means a "valid email" is formatted like:
-# [any alphanumeric characters]@[any alphanumeric characters].[any alphanumeric characters]
-VALID_EMAIL_PATTERN = '[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+'
-new_email = input("Enter email: ")
+# requires pip
+import mysql.connector
 
-if (re.search(VALID_EMAIL_PATTERN, new_email) != None):
-    print("valid email!")
-else:
-    print("invalid email")
+def validate_email(email: str) -> list:
+    # email requirements:
+        # [any alphanumeric characters]@[any alphanumeric characters].[any alphanumeric characters]
+    VALID_EMAIL_PATTERN = '[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+'
+    is_valid = False
+    info_msg = ""
 
-# password validation
-# a valid password requires at least one letter (in both cases) and at least one number
-# support for special characters will come later
-# (and also a better way to code this)
-while True:
-    new_password = input("Enter password: ")
-    if (len(new_password) < 8):
-        print("min password length: 8")
-    elif (re.search("[0-9]+", new_password) is None):
-        print("password needs at least one number")
-    elif (re.search("[a-z]+", new_password) is None):
-        print("password needs at least one lowercase letter")
-    elif (re.search("[A-Z]+", new_password) is None):
-        print("password needs at least one uppercase letter")
+    if (re.search(VALID_EMAIL_PATTERN, email) != None):
+        is_valid = True
     else:
-        print("valid password created")
-        break
+        info_msg = "Invalid email format"
+
+    return [info_msg, is_valid]
+
+def validate_password(password: str) -> list:
+    # password requirements:
+        # >8 alphanumerical characters
+        # >=1 (upper & lower)case letters
+        # >=1 number
+        # >=1 special character (TODO)
+    # (and also a better way to code this)
+
+    is_valid = False
+    info_msg = ""
+
+    if (len(password) < 8):
+        info_msg = "Password needs a minimum length of 8 characters"
+    elif (re.search("[0-9]+", password) is None):
+        info_msg = "Passwords needs at least one number"
+    elif (re.search("[a-z]+", password) is None):
+        info_msg = "Password needs at least one lowercase letter"
+    elif (re.search("[A-Z]+", password) is None):
+        info_msg = "Password needs at least one uppercase letter"
+    else:
+        is_valid = True
+        
+    return [info_msg, is_valid]
+
+def sign_in() -> bool:
+    new_email = input("Enter your email: ")
+    new_password = input("Enter your password: ")
+    
+    while True:
+        email_info_msg, is_email_valid = validate_email(new_email)
+        pw_info_msg, is_pw_valid = validate_password(new_password)
+
+        if (is_email_valid == True and is_pw_valid == True):
+            break
+        else:
+            if not is_email_valid:
+                print("\n"+email_info_msg)
+                new_email = input("Enter your email: ")
+            if not is_pw_valid:
+                print("\n"+pw_info_msg)
+                new_password = input("Enter your password: ")
+
+        
+    try:
+        conn = mysql.connector.connect(user=USER, 
+                                password=PASSWORD,
+                                host=HOST,
+                                database=DATABASE)
+        cur = conn.cursor()
+        print("Connected!")
+    except mysql.connector.Error as err:
+        print("Connection error:", err)
+
+    enc = hashlib.md5(new_password.encode()).hexdigest()
+    
+    cur.execute("INSERT INTO user_creds (email, password) VALUES (%s, %s)", (new_email, enc))
+    conn.commit()
+    conn.close()
+
+def main():
+    sign_in()
+
+if __name__ == "__main__":
+    main()
