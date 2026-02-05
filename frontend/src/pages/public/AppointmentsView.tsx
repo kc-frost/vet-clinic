b98dcf442f35def150d124b3e9b377dd2b333ea5
k128c <aeth.kr@gmail.com>
2026-02-05 01:38:40 -0600

    Add AppointmentsView.tsx

---
+++ frontend/src/pages/public/AppointmentsView.tsx
@@ -0,0 +1,18 @@
+import "../../styles/home.css";
+
+export default function Appointments() {
+  return (
+    <div className="appointments">
+      <div className="appointmentsInner">
+        <section className="hero">
+          <p className="kicker">Veterinary Clinic + Doggy Daycare</p>
+          <h1 className="title">View All Appointments</h1>
+          <p className="subtitle">
+            Please make an appointment with the following prompts.
+            
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+}
