63fb5694acf839512647c2ea0b3952e2188133e7
Rich93147 <richardwalker5357@gmail.com>
2026-02-04 19:21:43 -0600

    Refactor AppRouter component for route structure
    Accidently pulled to main, reverting back

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -14,24 +14,24 @@ import Inventory from "../pages/staff/Inventory";
 export default function AppRouter() {
   return (
     <BrowserRouter>
-  <Routes>
-    {/* Public site routes w/ public header/footer */}
-    <Route path="/" element={<PublicLayout />}>
-      <Route index element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="appointments" element={<Appointments />} />
-    </Route>
+      <Routes>
+        {/* Public site routes w/  public header/footer */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
 
-    {/* Staff routes */}
-    <Route path="/staff" element={<StaffLayout />}>
-      <Route index element={<Navigate to="inventory" replace />} />
-      <Route path="inventory" element={<Inventory />} />
-    </Route>
+        {/* Staff routes, separate layout w/ no public header/footer) */}
+        <Route path="/staff" element={<StaffLayout />}>
+          {/* If user goes to /staff, send them to /staff/inventory */}
+          <Route index element={<Navigate to="inventory" replace />} />
+          <Route path="inventory" element={<Inventory />} />
+        </Route>
 
-    {/* Catches any unknown URL redirected home */}
-    <Route path="*" element={<Navigate to="/" replace />} />
-  </Routes>
-</BrowserRouter>
+        {/* Catches any unknown URL redirected home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
